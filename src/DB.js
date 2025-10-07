5e4a08dadfff35bd56c27291b168382995ae565c
Stiffler-A <styphler17@gmail.com>
2025-10-07 14:40:43 +0200

    Création d'accès aux données DB.js

---
+++ src/DB.js
@@ -0,0 +1,60 @@
+// class DB
+export default class DB  {
+  static setApiURL(data) {
+    this.apiURL = data
+  }
+  
+  static async findAll() {
+    const response = await fetch(this.apiURL)
+
+    return response.json()
+  }
+  
+  static async create(contact) {
+    const res = await fetch(this.apiURL, {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        firstname: contact.firstname,
+        lastname:  contact.lastname,
+        email:     contact.email,
+      }),
+    })
+    return res.json()
+  }
+
+  // GET /firstname/:id
+  static async findById(id) {
+    const res = await fetch(this.apiURL + '/' + id, {
+
+      headers: { 'Accept': 'application/json' }
+    })
+    return res.json()
+  }
+
+  // PUT /firstname/:id
+  static async findByIdAndUpdate(id, partial) {
+    const res = await fetch(this.apiURL + '/' + id, {
+
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(partial),
+    })
+    return res.json()
+  }
+
+  static async findByIdAndRemove(id) {
+    const res = await fetch(this.apiURL + '/' + id, {
+
+      method: 'DELETE',
+    })
+    return res.json()
+  }
+}
